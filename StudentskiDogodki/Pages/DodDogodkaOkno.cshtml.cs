b1c0313b037c61f2d767791b18176c3fa34c2063
GrudenD <148212928+GrudenD@users.noreply.github.com>
2024-01-24 21:57:41 +0100

    Dodan dodajanje dogodka
    Impementriano dodajanje dogodka

--- StudentskiDogodki/Pages/DodDogodkaOkno.cshtml.cs
+++ StudentskiDogodki/Pages/DodDogodkaOkno.cshtml.cs
@@ -6,19 +6,30 @@ namespace StudentskiDogodki.Pages
     public class DodDogodkaOkno : PageModel
     {
         private BazaContext _db;
-
+       
         public DodDogodkaOkno(BazaContext db)
         {
             _db = db;
         }
 
-       
+        public void DodajaDogodkov()
+        {
+             Dogodek dogodek = new Dogodek("Dodani dogodek",new DateTime(2024,1,25,22,00,00),"Maribor trg","Jaz, Ti, Zidana marela","/Slike/dodan.jpg");
+            _db.Dogodki.Add(dogodek);
+            _db.SaveChanges();
+        }
 
         private Dogodek dogodek;
 
         private IndexModel glavnoOkno;
         public void OnGet()
         {
+            
+        }
+
+        public void OnPost()
+        {
+            DodajaDogodkov();
         }
     }
 }
